3b03bf104147dad6e7c110755caf1a82f83166b8
Ruud Kamphuis <ruudk@mphuis.com>
2017-02-06 11:52:59 +0100

    Add descriptive error message to ChainException
    We've seen this exceptions a couple of times in Sentry popping up. Since the exception is actually a holder of multiple exceptions, it was not clear what actually caused the issue.

---
+++ tests/Tests/Exception/ChainExceptionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Exchanger\Tests\Exception;
+
+use Exchanger\Exception\ChainException;
+use Exchanger\Exception\InternalException;
+
+/**
+ * @covers \Exchanger\Exception\ChainException
+ */
+class ChainExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ChainException
+     */
+    private $chainException;
+
+    /**
+     * @var \Exception
+     */
+    private $exception1;
+
+    /**
+     * @var \Exception
+     */
+    private $exception2;
+
+    public function setUp()
+    {
+        $this->exception1 = new InternalException('Something bad happened.');
+        $this->exception2 = new \Exception('General exception.');
+
+        $this->chainException = new ChainException(array(
+            $this->exception1,
+            $this->exception2,
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_have_a_descriptive_error_message()
+    {
+        $this->assertSame("The chain resulted in 2 exception(s):\r\nExchanger\\Exception\\InternalException: Something bad happened.\r\nException: General exception.", $this->chainException->getMessage());
+        $this->assertCount(2, $this->chainException->getExceptions());
+    }
+}
