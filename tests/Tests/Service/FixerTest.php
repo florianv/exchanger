2332da071eb81fe640bb3b6fca7dd3cf31991947
Dimitar Dinchev <dinchev.dimitar@gmail.com>
2018-04-16 16:43:38 +0300

    Require fixer.io module to receive access_key.

--- tests/Tests/Service/FixerTest.php
+++ tests/Tests/Service/FixerTest.php
@@ -22,7 +22,7 @@ class FixerTest extends ServiceTestCase
      */
     public function it_supports_all_queries()
     {
-        $service = new Fixer($this->getMock('Http\Client\HttpClient'));
+        $service = new Fixer($this->getMock('Http\Client\HttpClient'), null, ['access_key' => 'x']);
 
         $this->assertTrue($service->supportQuery(new ExchangeRateQuery(CurrencyPair::createFromString('USD/EUR'))));
         $this->assertTrue($service->supportQuery(new HistoricalExchangeRateQuery(CurrencyPair::createFromString('EUR/USD'), new \DateTime())));
@@ -34,10 +34,10 @@ class FixerTest extends ServiceTestCase
      */
     public function it_throws_an_exception_with_error_response()
     {
-        $uri = 'https://api.fixer.io/latest?base=USD';
+        $uri = 'https://api.fixer.io/latest?base=USD&access_key=x';
         $content = file_get_contents(__DIR__.'/../../Fixtures/Service/Fixer/error.json');
 
-        $service = new Fixer($this->getHttpAdapterMock($uri, $content));
+        $service = new Fixer($this->getHttpAdapterMock($uri, $content), null, ['access_key' => 'x']);
         $service->getExchangeRate(new ExchangeRateQuery(CurrencyPair::createFromString('USD/EUR')));
     }
 
@@ -46,10 +46,10 @@ class FixerTest extends ServiceTestCase
      */
     public function it_fetches_a_rate()
     {
-        $uri = 'https://api.fixer.io/latest?base=EUR';
+        $uri = 'https://api.fixer.io/latest?base=EUR&access_key=x';
         $content = file_get_contents(__DIR__.'/../../Fixtures/Service/Fixer/latest.json');
 
-        $service = new Fixer($this->getHttpAdapterMock($uri, $content));
+        $service = new Fixer($this->getHttpAdapterMock($uri, $content), null, ['access_key' => 'x']);
         $rate = $service->getExchangeRate(new ExchangeRateQuery(CurrencyPair::createFromString('EUR/CHF')));
 
         $this->assertEquals('1.0933', $rate->getValue());
@@ -61,11 +61,11 @@ class FixerTest extends ServiceTestCase
      */
     public function it_fetches_a_historical_rate()
     {
-        $uri = 'https://api.fixer.io/2000-01-03?base=USD';
+        $uri = 'https://api.fixer.io/2000-01-03?base=USD&access_key=x';
         $content = file_get_contents(__DIR__.'/../../Fixtures/Service/Fixer/historical.json');
         $date = new \DateTime('2000-01-03');
 
-        $service = new Fixer($this->getHttpAdapterMock($uri, $content));
+        $service = new Fixer($this->getHttpAdapterMock($uri, $content), null, ['access_key' => 'x']);
         $rate = $service->getExchangeRate(new HistoricalExchangeRateQuery(CurrencyPair::createFromString('USD/AUD'), $date));
 
         $this->assertEquals('1.5209', $rate->getValue());
