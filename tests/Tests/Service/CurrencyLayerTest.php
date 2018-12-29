98898bc1736e47801c01fb5e5025e02a7c1d56ba
florianv <voutzinos.florian@gmail.com>
2018-12-29 16:46:34 +0100

    Starting to add strict types

--- tests/Tests/Service/CurrencyLayerTest.php
+++ tests/Tests/Service/CurrencyLayerTest.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Exchanger.
  *
- * (c) Pascal Hofmann <mail@pascalhofmann.de>
+ * (c) Florian Voutzinos <florian@voutzinos.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
