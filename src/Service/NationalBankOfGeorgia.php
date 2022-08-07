0dbf0a8e10a6db7580cac8a7ee510d5eba54c581
Ugur Ozkan <uguurozkan@gmail.com>
2022-08-07 17:16:15 +0300

    Implement NationalBankOfGeorgia service

--- src/Service/NationalBankOfGeorgia.php
+++ src/Service/NationalBankOfGeorgia.php
@@ -2,9 +2,14 @@
 
 namespace Exchanger\Service;
 
+use DateTime;
+use DateTimeInterface;
+use Exception;
 use Exchanger\Contract\ExchangeRate;
 use Exchanger\Contract\ExchangeRateQuery;
 use Exchanger\Contract\HistoricalExchangeRateQuery;
+use Exchanger\Exception\UnsupportedCurrencyPairException;
+use Exchanger\StringUtil;
 
 /**
  * National Bank of Georgia Service.
@@ -15,11 +20,14 @@ final class NationalBankOfGeorgia extends HttpService
 {
     use SupportsHistoricalQueries;
 
+    private const BASE_URL = 'https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/en/json';
+
     /**
      * @inheritDoc
      */
     public function getName(): string
     {
+        return 'national_bank_of_georgia';
     }
 
     /**
@@ -27,19 +35,73 @@ final class NationalBankOfGeorgia extends HttpService
      */
     public function supportQuery(ExchangeRateQuery $exchangeQuery): bool
     {
+        return 'GEL' === $exchangeQuery->getCurrencyPair()->getQuoteCurrency();
     }
 
     /**
      * @inheritDoc
+     * @throws UnsupportedCurrencyPairException
      */
     protected function getLatestExchangeRate(ExchangeRateQuery $exchangeQuery): ExchangeRate
     {
+        return $this->doCreateRate($exchangeQuery);
     }
 
     /**
      * @inheritDoc
+     * @throws UnsupportedCurrencyPairException
      */
     protected function getHistoricalExchangeRate(HistoricalExchangeRateQuery $exchangeQuery): ExchangeRate
     {
+        return $this->doCreateRate($exchangeQuery, $exchangeQuery->getDate());
+    }
+
+    /**
+     * Creates the rate.
+     *
+     * @param ExchangeRateQuery $exchangeQuery
+     * @param DateTimeInterface|null $requestedDate
+     *
+     * @return ExchangeRate
+     * @throws UnsupportedCurrencyPairException
+     * @throws Exception
+     */
+    private function doCreateRate(ExchangeRateQuery $exchangeQuery, DateTimeInterface $requestedDate = null): ExchangeRate
+    {
+        $currencyPair = $exchangeQuery->getCurrencyPair();
+
+        $content = $this->request($this->buildUrl($requestedDate));
+        $element = StringUtil::jsonToArray($content)[0];
+
+        $date = new DateTime((string) $element['date']);
+
+        $currencyInfo = array_values(array_filter($element['currencies'], function ($currency) use ($currencyPair) {
+            return $currency['code'] === $currencyPair->getBaseCurrency();
+        }));
+        if (!empty($currencyInfo)) {
+            $rate = (float) $currencyInfo[0]['rate'];
+            $unit = (int) $currencyInfo[0]['quantity'];
+
+            return $this->createRate($currencyPair, ($rate / $unit), $date);
+        }
+
+        throw new UnsupportedCurrencyPairException($currencyPair, $this);
+    }
+
+    /**
+     * Builds the url.
+     *
+     * @param DateTimeInterface|null $requestedDate
+     *
+     * @return string
+     */
+    private function buildUrl(DateTimeInterface $requestedDate = null): string
+    {
+        $date = '';
+        if (!is_null($requestedDate)) {
+            $date = '?date=' . $requestedDate->format('Y-m-d');
+        }
+
+        return self::BASE_URL . $date;
     }
 }
